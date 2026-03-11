03e320e533eec16e8c02f2ae8f0bb1bfbfc5a50b
DianaKhramova <98611007+DianaKhramova@users.noreply.github.com>
2026-03-12 00:48:25 +0300

    Добавлена лабораторная работа №3

---
+++ lab3/blog/articles/views.py
@@ -0,0 +1,6 @@
+# Create your views here.
+from django.shortcuts import render
+from models import Article
+
+def archive(request):
+    return render(request, 'archive.html', {"posts": Article.objects.all()})
